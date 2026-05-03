d4345397b343c9e5e2fda4670e810d9a3e72c6d5
Bhoomi-Chaudhary <bhoomic773@gmail.com>
2026-05-04 02:46:08 +0530

    Before multi-admin system

--- models/AdminConfig.ts
+++ models/AdminConfig.ts
@@ -1,10 +1,14 @@
-// models/AdminConfig.js
 import mongoose from "mongoose";
 
-const AdminConfigSchema = new mongoose.Schema({
+interface IAdminConfig {
+  key: string;
+  value: string;
+}
+
+const AdminConfigSchema = new mongoose.Schema<IAdminConfig>({
   key:   { type: String, required: true, unique: true },
   value: { type: String, required: true },
 }, { timestamps: true });
 
 export default mongoose.models.AdminConfig ||
-  mongoose.model("AdminConfig", AdminConfigSchema);
+  mongoose.model<IAdminConfig>("AdminConfig", AdminConfigSchema);
