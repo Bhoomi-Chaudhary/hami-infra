adef9fe18af2c530675b297f3fd93d7a22dd6df6
Bhoomi-Chaudhary <bhoomic773@gmail.com>
2026-06-13 19:06:24 +0530

    Address on maps

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,22 +47,24 @@ export default function Footer() {
         {/* CONTACT */}
         <div>
           <h3 className="font-semibold mb-3">Contact</h3>
-          <p className="text-gray-300">Phone:
-            <a href="tel:+918796255342" >
+          <p className="text-gray-300">Phone: 
+            <a href="tel:+918796255342" className="hover:text-[#D96A1A]">
               +91 87962 55342
             </a>
           </p>
-          <p className="text-gray-300">Email:
-            <a href="mailto:info@hamiinfra.com" >
+          <p className="text-gray-300">Email: 
+            <a href="mailto:info@hamiinfra.com" className="hover:text-[#D96A1A]">
               info@hamiinfra.com
             </a>
           </p>
-          <p className="text-gray-300">Email:
-            <a href="mailto:infohamiinfra@gmail.com" >
+          <p className="text-gray-300">Email: 
+            <a href="mailto:infohamiinfra@gmail.com" className="hover:text-[#D96A1A]">
               infohamiinfra@gmail.com
             </a>
           </p>
-          <p className="text-gray-300 mt-2">Address: Govindpur, PARIKRAMMA MARG, Mathura, Mathura, Mathura- 281001, Uttar Pradesh</p>
+          <p className="text-gray-300 mt-2">Address: <Link href="https://www.google.com/maps/place/Govindpur,+PARIKRAMMA+MARG,+Mathura,+Mathura,+Mathura-+281001,+Uttar+Pradesh/@27.492187,77.672882,17z/data=!3m1!4b1!4m6!3m5!1s0x3974e7f8c9d2a6d5:0x7e8f8f8f8f8f8f8f!8m2!3d27.492187!4d77.672882!16s%2Fg%2F11c5v5v5v5" target="_blank" rel="noopener noreferrer" className="hover:text-[#D96A1A] cursor-pointer">
+            Govindpur, PARIKRAMMA MARG, Mathura, Mathura, Mathura- 281001, Uttar Pradesh
+          </Link></p>
         </div>
 
       </div>
